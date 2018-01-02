a4b2070223253d6d2784cb5701c385af5c349fe7
Andrea Tosatto <andrea.tosy@gmail.com>
2018-01-02 23:03:13 +0100

    Upgraded and fixed test suite

--- molecule/common/tests/test_docker_engine.py
+++ molecule/common/tests/test_docker_engine.py
@@ -1,17 +1,19 @@
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    '.molecule/ansible_inventory.yml').get_hosts('all')
+
+debian_os = ['debian', 'ubuntu']
+rhel_os = ['redhat', 'centos']
 
 
-def test_docker_repository_exists(File, SystemInfo):
+def test_docker_repository_exists(host):
 
     f = None
-    dist = SystemInfo.distribution
-    if dist == 'debian' or dist == 'ubuntu':
-        f = File('/etc/apt/sources.list.d/docker.list')
-    if dist == 'redhat' or dist == 'centos' or dist == 'fedora':
-        f = File('/etc/yum.repos.d/docker.repo')
+    if host.system_info.distribution.lower() in debian_os:
+        f = host.file('/etc/apt/sources.list.d/docker.list')
+    if host.system_info.distribution.lower() in rhel_os:
+        f = host.file('/etc/yum.repos.d/docker.repo')
 
     assert f.exists
     assert f.user == 'root'
@@ -19,19 +21,18 @@ def test_docker_repository_exists(File, SystemInfo):
     assert oct(f.mode) == '0644'
 
 
-def test_docker_engine_installed(Package):
+def test_docker_engine_installed(host):
 
-    assert Package('docker-engine').is_installed
+    assert host.package('docker-engine').is_installed
 
 
-def test_docker_service(Service, Command, SystemInfo):
+def test_docker_service(host):
 
     # Testinfra fails on Ubuntu trying to use systemd to get
     # the status of the Docker daemon
-    dist = SystemInfo.distribution
-    if dist == 'debian' or dist == 'ubuntu':
-        Command("/etc/init.d/docker status").rc == 0
+    if host.system_info.distribution.lower() in debian_os:
+        host.run("/etc/init.d/docker status").rc == 0
     else:
-        s = Service('docker')
+        s = host.service('docker')
         assert s.is_running
         assert s.is_enabled
--- molecule/swarm-cluster/tests/test_swarm_cluster.py
+++ molecule/swarm-cluster/tests/test_swarm_cluster.py
@@ -1,7 +1,7 @@
 import testinfra.utils.ansible_runner
 
 runner = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory')
+    '.molecule/ansible_inventory.yml')
 
 ALL_HOSTS = runner.get_hosts('all')
 MANAGER_HOSTS = runner.get_hosts('docker_swarm_manager')
@@ -10,19 +10,20 @@ WORKER_HOSTS = runner.get_hosts('docker_swarm_worker')
 testinfra_hosts = ALL_HOSTS
 
 
-def test_docker_swarm_enabled(Command):
+def test_docker_swarm_enabled(host):
 
-    assert 'Swarm: active' in Command.check_output('docker info')
+    assert 'Swarm: active' in host.check_output('docker info')
 
 
-def test_docker_swarm_status(Command, TestinfraBackend):
+def test_docker_swarm_status(host):
 
-    docker_info = Command.check_output('docker info')
+    docker_info = host.check_output('docker info')
+    hostname = host.check_output('hostname -s')
 
-    if TestinfraBackend.get_hostname() in MANAGER_HOSTS:
+    if hostname in MANAGER_HOSTS:
         assert 'Is Manager: true' in docker_info
         assert 'Nodes: 3' in docker_info       # the test cluster is of 3 nodes
         assert 'Managers: 2' in docker_info    # with 2 managers
 
-    elif TestinfraBackend.get_hostname() in WORKER_HOSTS:
-        assert 'Is Manager: false' in Command.check_output('docker info')
+    elif hostname in WORKER_HOSTS:
+        assert 'Is Manager: false' in docker_info
