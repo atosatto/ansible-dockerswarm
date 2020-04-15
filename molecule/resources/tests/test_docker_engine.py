74681efad33cfeb244ab1ca43fde94063731f97e
Andrea Tosatto <andrea.tosy@gmail.com>
2020-04-15 11:16:36 +0200

    Improve docker configuration folder permissions

--- molecule/resources/tests/test_docker_engine.py
+++ molecule/resources/tests/test_docker_engine.py
@@ -4,8 +4,20 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-debian_os = ['debian', 'ubuntu']
-rhel_os = ['redhat', 'centos']
+
+def test_docker_ce_config(host):
+
+    d = host.file('/etc/docker')
+    assert d.exists
+    assert d.user == 'root'
+    assert d.group == 'root'
+    assert d.mode == 0o755
+
+    f = host.file('/etc/docker/daemon.json')
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    assert f.mode == 0o640
 
 
 def test_docker_ce_stable_repository_exists(host):
@@ -27,13 +39,8 @@ def test_docker_ce_installed(host):
     assert host.package('docker-ce').is_installed
 
 
-def test_docker_service(host):
+def test_docker_ce_service(host):
 
-    # Testinfra fails on Ubuntu trying to use systemd to get
-    # the status of the Docker daemon
-    if host.system_info.distribution.lower() in debian_os:
-        host.run("/etc/init.d/docker status").rc == 0
-    else:
-        s = host.service('docker')
-        assert s.is_running
-        assert s.is_enabled
+    s = host.service('docker')
+    assert s.is_running
+    assert s.is_enabled
